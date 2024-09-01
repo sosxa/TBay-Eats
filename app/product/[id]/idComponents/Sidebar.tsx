c6b70d36cb528408d6b7cfcb861d08e74f0befe9
Jose Sosa <109876904+sosxa@users.noreply.github.com>
2024-09-01 17:25:15 -0400

    Update Sidebar.tsx

--- app/product/[id]/idComponents/Sidebar.tsx
+++ app/product/[id]/idComponents/Sidebar.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { XMarkIcon, TrashIcon, PencilIcon } from '@heroicons/react/24/outline';
 import { CartItem } from './CartItem';
 import CheckoutModal from './CheckOutModal';
-import { useRouter } from 'next/navigation'; // Import useRouter
+import { useRouter } from 'next/navigation';
 
 interface SidebarProps {
     isOpen: boolean;
@@ -12,21 +12,26 @@ interface SidebarProps {
     onUpdateItemQuantity: (itemId: string, quantity: number) => void;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, cartItems, onRemoveItem, onUpdateItemQuantity }) => {
+const Sidebar: React.FC<SidebarProps> = ({
+    isOpen,
+    onClose,
+    cartItems,
+    onRemoveItem,
+    onUpdateItemQuantity
+}) => {
     const [editableItemId, setEditableItemId] = useState<string | null>(null);
     const [tempQuantity, setTempQuantity] = useState<number | null>(null);
-    const [isCheckoutOpen, setIsCheckoutOpen] = useState<boolean>(false); // State to manage CheckoutModal visibility
-    const router = useRouter(); // Initialize useRouter
+    const [isCheckoutOpen, setIsCheckoutOpen] = useState<boolean>(false);
+    const router = useRouter();
 
     useEffect(() => {
-        // Add or remove the "no-scroll" class on body based on sidebar state
+        // Toggle no-scroll class on body based on sidebar state
         if (isOpen) {
             document.body.classList.add('no-scroll');
         } else {
             document.body.classList.remove('no-scroll');
         }
 
-        // Cleanup on component unmount
         return () => {
             document.body.classList.remove('no-scroll');
         };
@@ -34,11 +39,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, cartItems, onRemoveI
 
     const handleQuantityChange = (itemId: string, newQuantity: number) => {
         // Ensure quantity is between 1 and 100
-        if (newQuantity < 1) {
-            newQuantity = 1;
-        } else if (newQuantity > 100) {
-            newQuantity = 100;
-        }
+        if (newQuantity < 1) newQuantity = 1;
+        if (newQuantity > 100) newQuantity = 100;
         onUpdateItemQuantity(itemId, newQuantity);
     };
 
@@ -60,22 +62,12 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, cartItems, onRemoveI
         setTempQuantity(null);
     };
 
-    const openCheckoutModal = () => {
-        setIsCheckoutOpen(true);
-    };
+    const openCheckoutModal = () => setIsCheckoutOpen(true);
+    const closeCheckoutModal = () => setIsCheckoutOpen(false);
 
-    const closeCheckoutModal = () => {
-        setIsCheckoutOpen(false);
-    };
+    const handleRedirect = () => router.push('/checkout');
 
-    const handleRedirect = () => {
-        router.push("/checkout"); // Use router.push to redirect
-    };
-
-    const handleOverlayClick = () => {
-        // Close the sidebar when the overlay is clicked
-        onClose();
-    };
+    const handleOverlayClick = () => onClose();
 
     const isCartEmpty = cartItems.length === 0;
 
@@ -86,10 +78,10 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, cartItems, onRemoveI
                 <div
                     className="fixed inset-0 bg-black opacity-50 z-40"
                     onClick={handleOverlayClick}
-                    aria-hidden="true" // Improve accessibility by hiding from screen readers
+                    aria-hidden="true"
                 />
             )}
-            
+
             {/* Sidebar */}
             <div
                 className={`fixed top-0 right-0 h-full bg-gray-800 bg-opacity-75 transition-transform transform ${isOpen ? 'translate-x-0 z-50' : 'translate-x-full'} w-80`}
@@ -108,32 +100,27 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, cartItems, onRemoveI
                         ) : (
                             cartItems.map(item => {
                                 const quantity = editableItemId === item.itemId ? tempQuantity ?? item.quantity : item.quantity;
-                                // Use price from cart item, adjusted for discount if active
-                                const itemPrice = item.activeDiscount ? item.discountPrice : parseFloat(item.price);
+                                const itemPrice = item.activeDiscount ? parseFloat(item.discountPrice) : parseFloat(item.price);
                                 const dynamicPrice = (itemPrice * quantity).toFixed(2);
 
                                 return (
-                                    <li key={`${item.itemId}-${item.quantity}`} className="border-b border-gray-200 py-2 flex items-center">
-                                        {/* Image */}
+                                    <li key={item.itemId} className="border-b border-gray-200 py-2 flex items-center">
                                         <img
                                             src={item.firstImgUrl}
                                             alt={item.itemName}
                                             className="w-16 h-16 object-cover mr-4"
                                         />
-                                        {/* Text Content */}
                                         <div className="flex-1">
                                             <p className="font-medium">{item.itemName}</p>
                                             <p className="text-gray-600">{item.filter} food</p>
-                                            {!item.activeDiscount && <p className="text-gray-700">${dynamicPrice}</p>}
-                                            {item.activeDiscount && <p className="text-gray-700">${dynamicPrice} (%{item.discountAmount} off)</p>}
+                                            <p className="text-gray-700">${dynamicPrice} {item.activeDiscount && `(%${item.discountAmount} off)`}</p>
 
-                                            {/* Quantity Controls */}
                                             <div className="flex items-center space-x-2">
                                                 {editableItemId === item.itemId ? (
                                                     <>
                                                         <input
                                                             type="number"
-                                                            value={tempQuantity !== null ? tempQuantity : ''}
+                                                            value={tempQuantity ?? ''}
                                                             min="1"
                                                             max="100"
                                                             onChange={(e) => setTempQuantity(Number(e.target.value))}
@@ -165,7 +152,6 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, cartItems, onRemoveI
                                                 )}
                                             </div>
                                         </div>
-                                        {/* Trash Can Icon */}
                                         <button
                                             onClick={() => onRemoveItem(item.itemId)}
                                             className="ml-4 text-red-500 hover:text-red-700"
@@ -178,8 +164,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, cartItems, onRemoveI
                         )}
                     </ul>
                     <button
-                        onClick={handleRedirect} // Updated to use handleRedirect
-                        disabled={isCartEmpty} // Disable button if cart is empty
+                        onClick={handleRedirect}
+                        disabled={isCartEmpty}
                         className={`mt-auto py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-custom-green ${isCartEmpty ? 'bg-gray-400 cursor-not-allowed' : 'bg-custom-green text-white hover:bg-custom-dark-green'}`}
                     >
                         Checkout
