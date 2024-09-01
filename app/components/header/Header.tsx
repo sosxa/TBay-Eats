d3555b8e6f88535f1506428e145afa7c34e3c15d
Jose Sosa <109876904+sosxa@users.noreply.github.com>
2024-09-01 18:39:30 -0400

    Update Header.tsx

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -5,13 +5,15 @@ import NavbarMobile from './headerComponents/NavbarMobile'
 import SearchBtn from './headerComponents/SearchBtn'
 
 const Header = () => {
-
+    const toggleSidebar = () => {
+        return;
+    }
     return (
 
         // <div className='w-auto z-auto bg-custom-green mb-[5rem]'>
         //     <div className='justify-center'>
         <>
-            <NavbarLaptop className='hidden lg:inline lg:mt-10' />
+            <NavbarLaptop className='hidden lg:inline lg:mt-10' onCartClick={toggleSidebar} />
             <NavbarMobile className='lg:hidden' />
         </>
         //  </div>
