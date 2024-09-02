1833eb93ccb068803c950467e70a9218e2f1bbf4
Jose Sosa <109876904+sosxa@users.noreply.github.com>
2024-09-01 20:58:25 -0400

    Update page.tsx

--- app/restaurant/page.tsx
+++ app/restaurant/page.tsx
@@ -1,14 +1,22 @@
+'use server';
 import React from 'react'
 import HeaderWithNav from '../components/header/HeaderWithNav'
 import FooterLayout from '../components/footerLayouts/FooterLayout'
 import ListingClient from './restaurantListingComponents/ListingClient'
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation'
 
-const page = () => {
+const page = async () => {
+  const supabase = createClient();
+  const { data: { session } } = await supabase.auth.getSession()
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <>
       <HeaderWithNav />
       <div>
-       <ListingClient />
+        <ListingClient />
       </div>
       <FooterLayout />
     </>
