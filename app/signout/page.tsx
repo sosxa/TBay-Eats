2a3bf67dd8a49ab19ef2f5b2c6b1e8b9fa3f937a
Jose Sosa <109876904+sosxa@users.noreply.github.com>
2024-09-01 20:55:49 -0400

    Update page.tsx

--- app/signout/page.tsx
+++ app/signout/page.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 import ClientRedirect from './zzz'
 import HeaderWithNav from '../components/header/HeaderWithNav'
 import FooterLayout from '../components/footerLayouts/FooterLayout'
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation'
 
-const page = () => {
+const page = async () => {
+
+    const supabase = createClient();
+    const { data: { session } } = await supabase.auth.getSession()
+    if (!session) {
+        redirect("/login");
+    }
+
     return (
         <>
             <HeaderWithNav />
