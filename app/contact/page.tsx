dd6a653b2d096c9680a1114b2f370497cf1ce6e1
Jose Sosa <109876904+sosxa@users.noreply.github.com>
2024-09-01 20:58:09 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,20 @@
+'use server';
 import React from 'react'
-
 import HeaderWithNav from '../components/header/HeaderWithNav';
 import FooterLayout from '../components/footerLayouts/FooterLayout';
-// import ContactPg from "../components/contactComponent/ContactPg";
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation'
 
-const page = () => {
+const page = async () => {
+    const supabase = createClient();
+    const { data: { session } } = await supabase.auth.getSession()
+    if (!session) {
+        redirect("/login");
+    }
     return (
         <div>
             <HeaderWithNav />
-{/*             <ContactPg /> */}
+            {/*             <ContactPg /> */}
             <FooterLayout />
         </div>
     )
