5074beb8b2da2d584cf133e5692f0995f910b8f9
Jose Sosa <109876904+sosxa@users.noreply.github.com>
2024-09-01 21:49:04 -0400

    Update fetchUserType.ts

--- app/components/header/headerComponents/fetchUserType.ts
+++ app/components/header/headerComponents/fetchUserType.ts
@@ -3,21 +3,6 @@ import { createClient } from '@/utils/supabase/server';
 
 export default async function fetchUserType() {
     const supabase = createClient();
-    const { data: user } = await supabase.auth.getUser();
-    const {
-        data: { session },
-    } = await supabase.auth.getSession();
-    console.log("user.user?.email")
-console.log(user.user?.email)
-
-    // const { data: profile, error } = await supabase
-    //     .from('profile')
-    //     .select(user)
-    //     .eq("email", email)
-    // if (error) {
-    //     throw error;
-    // }
-    // const userType = profile[0]?.user;
-
-    // return userType;
+    const { data: userData } = await supabase.auth.getUser();
+    return userData.user;
 }
